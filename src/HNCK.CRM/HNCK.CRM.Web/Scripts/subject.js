39b179170c8305da3f9e4224e08c8973be97b90f
David Spišák <david.spisak@slovatech.sk>
2022-03-15 21:41:58 +0100

    add docx tamplates

--- src/HNCK.CRM/HNCK.CRM.Web/Scripts/subject.js
+++ src/HNCK.CRM/HNCK.CRM.Web/Scripts/subject.js
@@ -34,6 +34,31 @@ $("#GenerateDocument_02").click(function () {
 	GenerateDocuments("02_spl_cudzinci.docx");
 });
 
+$("#GenerateDocument_03").click(function () {
+	GenerateDocuments("03_vyhlasenie_jedineho_spolocnika.docx");
+});
+
+$("#GenerateDocument_04").click(function () {
+	GenerateDocuments("04_spravca_vkladu.docx");
+});
+
+$("#GenerateDocument_05").click(function () {
+	GenerateDocuments("05_zakladatelska_listina.docx");
+});
+
+$("#GenerateDocument_06").click(function () {
+	GenerateDocuments("06_splnomocnenie.docx");
+});
+
+$("#GenerateDocument_07").click(function () {
+	GenerateDocuments("07_spl_danovi_autori.docx");
+});
+
+$("#GenerateDocument_08").click(function () {
+	GenerateDocuments("08_vyhlasenie_do_or.docx");
+});
+
+
 
 function subjectDelete(subject) {
 	return kendo.format('<div class="d-flex justify-content-center"><a rel="nofollow" href="/Subject/Delete/{0}"><i class="hnck-text-red fas fa-trash-alt"></i></a></div>', subject.IdSubject);
